7b560ce7080e336413571c972f6ca0f544c16429
NethePaul <paul04nethe@gmail.com>
2024-09-05 09:10:46 +0200

    fixed most translation errors

--- python/autotranslate.py
+++ python/autotranslate.py
@@ -5,16 +5,55 @@ import csv
 import os.path
 import shutil
 import sys
-from libretranslatepy import LibreTranslateAPI
+import json
+from urllib import request, parse
 
+
+
+class LibreTranslateAPI:
+    DEFAULT_URL = "http://127.0.0.1:5000"
+
+    def __init__(self, url=None, api_key=None):
+        """Create a LibreTranslate API connection.
+
+        Args:
+            url (str): The url of the LibreTranslate endpoint.
+            api_key (str): The API key.
+        """
+        self.url = LibreTranslateAPI.DEFAULT_URL if url is None else url
+        self.api_key = api_key
+
+        # Add trailing slash
+        assert len(self.url) > 0
+        if self.url[-1] != "/":
+            self.url += "/"
+
+    def translate(self, q, source="en", target="es"):
+        """Translate string
+
+        Args:
+            q (str): The text to translate
+            source (str): The source language code (ISO 639)
+            target (str): The target language code (ISO 639)
+
+        Returns:
+            str: The translated text
+        """
+        url = self.url + "translate"
+        params = {"q": q, "source": source, "target": target, "format":"html"}
+        if self.api_key is not None:
+            params["api_key"] = self.api_key
+        url_params = parse.urlencode(params)
+        req = request.Request(url, data=url_params.encode())
+        response = request.urlopen(req)
+        response_str = response.read().decode()
+        return json.loads(response_str)["translatedText"]
 
 """
     entfernt latex, maxima, und html von den Text von einer Stack Frage
     sodass es automatisch übersetzbar ist
 """
-def rm_latex(s,x):
-    xs=["X","#","","##","!","@"]
-    x=xs[x%len(xs)]
+def rm_latex(s):
     l=len(s)
     i=0
     j=0
@@ -29,8 +68,8 @@ def rm_latex(s,x):
         {'i':1,'start':["<style","<script","[[jsxgraph"],'end':["</style>","</script>","/>","[[/jsxgraph]]"]},
         {'i':2,'start':["\\begin"],'end':["\\end"]},
         {'i':3,'start':["<"],'end':[">"]},
-        {'i':4,'start':["&"],'end':[";"]},
-        {'i':5,'start':["{","[["],'end':["}","]]"]}
+        # {'i':4,'start':["&"],'end':[";"]},
+        {'i':4,'start':["{","[["],'end':["}","]]"]}
     ]
     out={'s':"",'count':0}
     while True:
@@ -40,7 +79,7 @@ def rm_latex(s,x):
             out['count']=j
             return out
         special=False
-        if level>0 and (s[i]=='"' or s[i]=="'"):
+        if level>0 and cstyle!=0 and cstyle!=2 and (s[i]=='"' or s[i]=="'"):
             if not brackets:
                 brackets=s[i]
             elif s[i]==brackets:
@@ -66,7 +105,7 @@ def rm_latex(s,x):
                             level=level-1
                             i=i+len(st)
                             if level == 0:
-                                out['s']=out['s']+f"[{x}{j}]"
+                                out['s']=out['s']+f'<x id="{j}"></x>'
                                 out[str(j)]=s[start:i]
                                 j=j+1
                             special=True
@@ -76,7 +115,7 @@ def rm_latex(s,x):
                 if not s[i+1].isalnum() or level!=0:
                     if level==0:
                         out[str(j)]=s[i]+s[i+1]
-                        out['s']=out['s']+f"[{x}{j}]"
+                        out['s']=out['s']+f'<x id="{j}"></x>'
                         j=j+1
                     i=i+2
                 else:
@@ -85,7 +124,7 @@ def rm_latex(s,x):
                     while i+1<l and s[i+1].isalnum():
                         i=i+1
                     i=i+1
-                    out['s']=out['s']+f"[{x}{j}]"
+                    out['s']=out['s']+f'<x id="{j}"></x>'
                     out[str(j)]=s[start:i]
                     j=j+1
         elif not special and level==0:
@@ -95,17 +134,15 @@ def rm_latex(s,x):
             i=i+1          
 
 
-def readd_latex(r,x):
-    xs=["X","#","","##","!","@"]
-    x=xs[x%len(xs)]
+def readd_latex(r):
     s=r['s']
     if r['count']==0:
         return s
     for i in range(r['count']):
-        c = s.count(f"[{x}{i}]")
+        c = s.count(f'<x id="{i}"></x>')
         if c!=1:
             return ""
-        s=s.replace(f"[{x}{i}]",r[str(i)])
+        s=s.replace(f'<x id="{i}"></x>',r[str(i)])
     return s
 
 
@@ -143,20 +180,20 @@ for filename in filenames:
     for zeile in zeilen:
         if not zeile['en']:
             en=""
-            for i in range(3):
-                sys.stdout.write(f" Nummer: {zeile['Nummer']}{i+1}/3\r")
-                sys.stdout.flush()
-                o=rm_latex(zeile['de'],i);
-            
-                # dbg=o['s']
-                if o['s']:
-                    o['s']=lt.translate(o['s'], "de", "en")
-                en=readd_latex(o,i)
-                if en:
-                    break
+            sys.stdout.write(f" Nummer: {zeile['Nummer']}\r")
+            sys.stdout.flush()
+            o=rm_latex(zeile['de']);
+        
+            dbg=o
+            de=o['s']
+            if o['s']:
+                o['s']=lt.translate(o['s'], "de", "en")
+            en=readd_latex(o)
             if not en:
+                o['s']='Translation Error\n'+de
+                en=readd_latex(o)
                 print(f"Fehler bei der Übersetzung von Nummer: {zeile['Nummer']}")
-                en='Translation Error'
+                
             zeile['en']=en
         writer.writerow(zeile)
     csvfile.close()
