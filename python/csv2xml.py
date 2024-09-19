4d856fc062ab47e82946646b164a494279674895
NethePaul <paul04nethe@gmail.com>
2024-09-19 14:07:06 +0200

    fixed duplicate [[input:ans]]

--- python/csv2xml.py
+++ python/csv2xml.py
@@ -3,8 +3,15 @@
 
 import glob
 import csv
+import re
+
 
 filenames=glob.glob('*.xml')
+ptn=r"""\[\[(?:validation|input|feedback):[a-zA-Z0-9_]*\]\]"""
+
+
+def wrap(str,lang):
+    return f'<span lang="{lang}" class="multilang">{str}</span>'
 
 for filename in filenames:
     if filename.endswith("_new.xml"):
@@ -28,10 +35,27 @@ for filename in filenames:
     
     #encoding="ISO-8859-1")
     reader = csv.DictReader(csvfile, delimiter=deli)
-    languages=[lang for lang in reader.fieldnames if lang!='Nummer']
+
+    error=False
     for row in reader:
-        new_str="".join([f'<span lang="{lang}" class="multilang">{row[lang]}</span>' for lang in languages if row[lang]])
+        found_de=re.findall(ptn,row['de'])
+        found_en=re.findall(ptn,row['en'])
+        split_de=[wrap(i,"de") for i in re.split(ptn,row['de'])]
+        split_en=[wrap(i,"en") for i in re.split(ptn,row['en'])]
+        if found_de!=found_en or len(split_de)!=len(split_en):
+            print(f"Fehler bei Nummer {row['Nummer']} : die [[validation:?]], [[input:?]], oder [[feedback:?]] passen nicht in beiden Übersetzungen zusammen")
+            print("Abbruch!")
+            error=True
+            break
+        print(len(found_de))
+        found_de.append('')
+        z=zip(found_de,split_de,split_en)        
+        
+        
+        new_str="".join(f"{sd}{se}{f}" for f,sd,se in z)
         inhalt=inhalt.replace(row['Nummer'],new_str)
+    if error:
+        continue
     csvfile.close()
 
     newfile=open(filename[:-4]+'_new.xml','w',newline='',encoding='utf8')
